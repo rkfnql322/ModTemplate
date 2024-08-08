9b678c1414252ee3d5ec5b7325edcb6b1ad26759
DartRuffian <skulldude327@gmail.com>
2024-08-08 11:16:45 -0500

    Added required CBA version

--- addons/main/script_mod.hpp
+++ addons/main/script_mod.hpp
@@ -2,7 +2,9 @@
 #define PREFIX MOD_PREFIX
 #define MOD_NAME MOD_TITLE
 #define AUTHOR "DartRuffian"
+
 #define REQUIRED_VERSION 2.16
+#define REQUIRED_CBA_VERSION {3,17,0}
 
 #include "script_version.hpp"
 
